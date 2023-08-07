4a205de45e89383f2fe2a74d9275307bf5e07baa
zmxing <191840273@smail.nju.edu.cn>
2023-08-07 20:40:16 +0800

    finish mario.py which has been checked

--- problemset/set6/sentimental-mario-more/mario.py
+++ problemset/set6/sentimental-mario-more/mario.py
@@ -1,1 +1,10 @@
-# TODO
+from cs50 import get_int
+
+n = get_int("Height: ")
+
+while n < 1 or n > 8:
+    n = get_int("Height: ")
+
+for i in range(n):
+    print(" " * (n - i - 1) + "#" * (i + 1) + "  " + "#" * (i + 1))
+
